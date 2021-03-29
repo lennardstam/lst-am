e938e2b4cbf3a2c94086ac004ceaeab5ab18e118
lennard <lennard@stam.cc>
2021-03-29 22:11:34 +0200

    clean up round 1

--- src/main/forms.py
+++ src/main/forms.py
@@ -1,24 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL, ValidationError
+from wtforms.validators import DataRequired, URL, Optional, length, Regexp, ValidationError
 
 from src.models import Link
 
 
-class UrlCreated(FlaskForm):
-    url_org = StringField(' Original URL ', validators=[DataRequired(), URL()], render_kw={"placeholder": "Shorten your link."})
-    url_short = StringField(' New URL ', render_kw={"placeholder": "..."})
-
-
 class UrlCreate(FlaskForm):
     url_org = StringField('Shorten', validators=[DataRequired(), URL()], render_kw={"placeholder": "https://..."})
-    url_short = StringField('New URL', render_kw={"placeholder": "."})
+    # url_short = StringField('New URL', validators=[Optional(strip_whitespace=True), length(max=10)], render_kw={"placeholder": "...."})
+    url_short = StringField('New URL', validators=[Optional(strip_whitespace=True), length(max=10), Regexp(r'^[\w.@+-]+$')], render_kw={"placeholder": "...."})
+    # url_short = StringField('New URL', validators=[Regexp(r'^[\w.@+-]+$'), Optional(strip_whitespace=True), length(max=10)], render_kw={"placeholder": "...."})
     submit = SubmitField('Submit')
 
 
 class LinkUpdate(FlaskForm):
     url_org = StringField('Original Url', validators=[DataRequired(), URL()])
-    url_short = StringField('Short Url', validators=[DataRequired()], render_kw={"placeholder": "."})
+    url_short = StringField('New URL', validators=[DataRequired(), Optional(strip_whitespace=True), length(max=10), Regexp(r'^[\w.@+-]+$')], render_kw={"placeholder": "...."})
+    # url_short = StringField('New URL', validators=[DataRequired(),Optional(strip_whitespace=True), length(max=10), Regexp(r'^[\w.@+-]+$')], render_kw={"placeholder": "."})
     submit = SubmitField('Update')
 
     # def validate_url(self, url_short):
